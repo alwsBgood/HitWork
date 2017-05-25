4fbb8825603d2c55f5618714d55c9bb0136a30cb
Dmitriy <DmitryChebotaru@GoITAdmins-iMac-7.local>
2017-05-25 14:10:25 +0300

    Yandex geoloc deletion

--- app/mail.php
+++ app/mail.php
@@ -49,6 +49,6 @@ $headers = "MIME-Version: 1.0" . PHP_EOL .
 'From: '.adopt($project_name).' <'.$admin_email.'>' . PHP_EOL .
 'Reply-To: '.$admin_email.'' . PHP_EOL;
 
-mail('belinines@mail.ru', adopt($form_subject), $message, $headers );
+mail('ievgenbelinin@gmail.com', adopt($form_subject), $message, $headers );
 mail('s.pekelna@gmail.com', adopt($form_subject), $message, $headers );
 mail($admin_email, adopt($form_subject), $message, $headers );
